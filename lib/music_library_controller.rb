2702ad18a0d764e5c50c0c0c3dcc43f91edbfbc3
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-05-24 19:46:51 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -22,8 +22,7 @@ end
 end 
 
 def list_songs 
-  prints all songs alphabetized by song name 
-  Song.all.each_with_index {|song,index| puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
+  Song.all.each_with_index{|song,index| puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
 end 
   
   
