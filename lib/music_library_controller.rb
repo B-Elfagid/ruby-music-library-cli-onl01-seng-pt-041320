5f48a16f19a2dafbdfaf5a5676416aad13fd01b1
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-05-24 22:54:10 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -90,16 +90,16 @@ songs_sorted_by_artist = Artist.all.sort_by do |artist|
     end 
   end 
   
-  def play_song
+  def play_song 
     puts "Which song number would you like to play?"
-    song_names = self.song_array
-    user_input = gets.chomp.to_i
-    if user_input > 0 && user_input <= self.library.size
-      chosen_input = song_names[user_input - 1]
-      chosen_input = name_extractor(chosen_input)[1]
-      song = Song.find_by_name(chosen_input)
-      puts "Playing #{song.name} by #{song.artist.name}" unless song == nil
+    list_of_songs = Song.all.sort{ |a,b| a.name <=> b.name }
+     input = gets.strip.to_i
+    if (1..Song.all.length).include?(input)
+      song = list_of_songs[input+2]
+      puts "Playing #{song.name} by #{song.artist.name}"
+      unless song == nil
     end
-    end
+  end
+end 
 
 end
