dcf83d0ed8ce164a575df9559782b05aac34aa5d
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-05-24 20:43:10 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -51,14 +51,14 @@ songs_sorted_by_artist = Artist.all.sort_by do |artist|
   end
   end
   
-  def list_genre
-    songs_sorted_by_genre = Genre.all.sort_by do |genre|
+  def list_genres
+   songs_sorted_by_genre = Genre.all.sort_by do |genre|
       genre.name
     end
-    
     songs_sorted_by_genre.each.with_index(1) do |genre,index|
-    puts "#{index}. #{genre.name}"
+      puts "#{index}. #{genre.name}"
+    end
   end
-end 
+
   
 end 
