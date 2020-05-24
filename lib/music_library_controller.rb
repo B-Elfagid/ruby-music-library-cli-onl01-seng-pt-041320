15c2b94ac8580121927c0749f72ac7cf8ae8a0fa
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-05-24 20:25:24 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,37 +1,38 @@
 class MusicLibraryController
-  
-  class MusicLibraryController
 
   def initialize(path = "./db/mp3s")
-    MusicImporter.new(path).import
+    new_importer_object = MusicImporter.new(path)
+    new_importer_object.import
   end
 
   def call
     input = ""
-    until input == "exit"
-      puts "Please enter action"
-      input = gets.chomp
+    while input != "exit"
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
+      input = gets.strip
+
       case input
       when "list songs"
         list_songs
-      when "list artists" 
+      when "list artists"
         list_artists
       when "list genres"
         list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
       when "play song"
         play_song
-      when "list artist"
-        list_artist
-      when "list genre"
-        list_genre
-      when "exit"
-        puts "Goodbye" 
-      else
-        puts "invalid action"
       end
-    end
-  end
 
-  def list_songs
-    Song.all.each_with_index {|song,index|puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
+    end
   end
