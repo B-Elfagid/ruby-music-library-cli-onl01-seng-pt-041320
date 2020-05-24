cee50c3023247ec0a556caf8a2ddda26120b1409
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-05-24 20:12:07 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -35,9 +35,7 @@ end
 end 
 end 
 
-def list_songs
- Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |song, index|
-        puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
- end
-end
+ def list_songs
+    Song.all.each_with_index {|song,index|puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
+  end
 end 
