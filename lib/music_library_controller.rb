53c046c98d6f6859fe9b8657eb1814efe7220def
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-05-24 20:17:08 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -18,30 +18,7 @@ puts "To play a song, enter 'play song'."
 puts "To quit, type 'exit'."
 puts "What would you like to do?"
 input = gets.strip
-case input
-when "list songs"
-list_songs
-when "list artists"
-list_artists
-when "list genres"
-list_genres
-when "list artist"
-list_songs_by_artist
-when "list genre"
-list_songs_by_genre
-when "play song"
-play_song
-when "list artist"
-list_artist
-when "list genre"
-list_genre
-when "exit"
-puts "Goodbye" 
-else
-puts "invalid action"
-end
-end
-end
+end 
 
 def list_songs
  Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |song, index|
