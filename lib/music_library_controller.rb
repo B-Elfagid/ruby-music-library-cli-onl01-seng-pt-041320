dc4b20132a2d32c09edf82b26556c99bfc110126
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-05-24 23:07:56 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -93,12 +93,7 @@ songs_sorted_by_artist = Artist.all.sort_by do |artist|
    def play_song
     puts "Which song number would you like to play?"
     list_of_songs = Song.all.sort{ |a, b| a.name <=> b.name }
-
-    input = gets.strip.to_i
-    if (1..Song.all.length).include?(input)
-      song = list_of_songs[input+2]
-      puts "Playing #{song.name} by #{song.artist.name}"
-    end
-  end
+    answer = gets.chomp.to_i
+    puts "Playing #{Song.all[answer-1].artist.name} - #{Song.all[answer-1].name} - #{Song.all[answer-1].genre.name}"
 end
 
