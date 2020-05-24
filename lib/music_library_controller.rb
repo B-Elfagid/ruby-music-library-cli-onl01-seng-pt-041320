26e1610143852e0aebc9082092b8d3f8d45c919a
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-05-24 22:56:12 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -95,12 +95,12 @@ songs_sorted_by_artist = Artist.all.sort_by do |artist|
     list_of_songs =  Song.all.sort{ |a, b| a.name <=> b.name }
 
     input = gets.strip.to_i
-    #if (input > 0) && (input <= list_of_songs.size)
+    if (input > 0) && (input <= list_of_songs.size)
     #if (input >= 1) && (input <= list_of_songs.size)
-    if (1..Song.all.length).include?(input)
+    #if (1..Song.all.length).include?(input)
       song = list_of_songs[input+2]
       puts "Playing #{song.name} by #{song.artist.name}"
     end
   end
-
 end
+end 
