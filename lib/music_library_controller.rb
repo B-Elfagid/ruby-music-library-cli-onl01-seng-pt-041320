5bf3f06f80660176cdd5e970bb46ed6c6e93b61b
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-05-24 20:18:51 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -18,10 +18,27 @@ puts "To play a song, enter 'play song'."
 puts "To quit, type 'exit'."
 puts "What would you like to do?"
 input = gets.strip
-end 
-
-def list_songs
- Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |song, index|
-        puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
- end
+case input
+when "list songs"
+list_songs
+when "list artists"
+list_artists
+when "list genres"
+list_genres
+when "list artist"
+list_songs_by_artist
+when "list genre"
+list_songs_by_genre
+when "play song"
+play_song
 end
+end
+end
+
+  def list_songs
+
+  Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
+  puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
+  end
+end
+
